8848406d5d9c8cdad0de7fc5147e9d8058bc09bf
JeongJunHyuk <wnsgur727@naver.com>
2018-11-21 03:17:07 +0900

    layout_Firsttouched

--- routes/club.js
+++ routes/club.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('club');
+  res.render('club',{title:'club', layout:'layouts/layout2'});
 });
+
 
 module.exports = router;
