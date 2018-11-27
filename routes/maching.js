10a0da80a5e7196a55c83076bd80544e1c20b662
JeongJunHyuk <wnsgur727@naver.com>
2018-11-27 19:12:10 +0900

    club_regi

--- routes/maching.js
+++ routes/maching.js
@@ -4,7 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('maching',{title:'maching',session:req.mysession,layout:'layouts/layout2'});
-  console.log("hhh"+"new"+req.test.is);
   
 });
 
