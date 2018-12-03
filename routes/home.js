4d2acdc71bd2f57851f1d615c0a478f10721d2f0
ahn7253 <pyungryulahn1009@gmail.com>
2018-12-03 20:33:08 +0900

    edit main page

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home',{session:req.mysession});
+  res.render('home',{title: 'home', session:req.mysession});
 });
 
 module.exports = router;
