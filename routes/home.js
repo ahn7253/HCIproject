d0ada961a540f5f0d5d8c789199aefd93a1414de
ahn7253 <pyungryulahn1009@gmail.com>
2018-12-10 19:59:39 +0900

    add search page

--- routes/home.js
+++ routes/home.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('home',{title: 'home', session:req.mysession});
 });
 
+router.get('/searchpage', function (req, res, next) {
+  res.render('searchpage/searchpage', { title: 'searchpage', session: req.mysession, layout: 'layouts/layout2' });
+});
+
 module.exports = router;
